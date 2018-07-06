b6e4b8761cefdad3312395f3013de76557bf6292
ghoshben <sg2052@cse.jgec.ac.in>
2018-07-06 16:37:54 +0530

    Update jiotoken.py

--- jiotoken.py
+++ jiotoken.py
@@ -1,36 +1,13 @@
 import requests
-import json
+import re
 import time
-
+url = "http://thoptv.win/_livetv-jasmine/Ten3_HD-600-smumcdnems01.m3u8"
 while True:
-    url = 'https://api.jio.com/v3/dip/user/unpw/verify'
-    payload = {
-        "identifier": "genious.bittu1@gmail.com",
-        "password": "shubham1#",
-        "rememberUser": "T",
-        "upgradeAuth": "Y",
-        "returnSessionDetails": "T",
-        "deviceInfo": {
-            "consumptionDeviceName": "HUAWEI LLD-AL10",
-            "info": {
-                "type": "android",
-                "androidId": ""
-            }
-        }
-    }
-    # Adding empty header as parameters are being sent in payload
-    headers = {'Content-Type': 'application/json',
-               'x-api-key': 'l7xxe10e037b4d014ed5b4df6344db3fbb01',
-               'Content-Length': '290',
-               'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 8.0.0; LLD-AL10 Build/HONORLLD-AL10)',
-               'Host': 'api.jio.com',
-               'Connection': 'Keep-Alive',
-               'Accept-Encoding': 'gzip'}
-    r = requests.post(url, data=json.dumps(payload), headers=headers)
-    js = r.json()
-    token =js["ssoToken"]
-    fh =open("jio.txt","w")
-    fh.write(token)
-    fh.close()
+    r =requests.get(url)
+    r =r.content
+    a = re.split("[?\"]",r)
+    token = a[2]
+    b =open("token.txt","w")
+    b.write(token)
+    b.close()
     time.sleep(300)
-    print("done)
